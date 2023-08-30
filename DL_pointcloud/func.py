efa561fe1cb7e4f15e8cc635c7205ec823e1e285
Колесников Дмитрий Андреевич <kolesnikovda@digtp.com>
2023-08-30 18:36:16 +0300

    train pointnet

--- DL_pointcloud/func.py
+++ DL_pointcloud/func.py
@@ -2,6 +2,13 @@ import plotly.graph_objects as go
 import plotly.express as px
 import matplotlib.pyplot as plt
 import numpy as np
+import itertools
+
+import torch
+import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
+
 
 
 def visualize_rotate(data):
@@ -49,3 +56,129 @@ def pcshow(xs,ys,zs):
                       color='DarkSlateGrey')),
                       selector=dict(mode='markers'))
     fig.show()
+
+
+########### АРХИТЕКТУРА СЕТИ ###############
+class Tnet(nn.Module):
+    def __init__(self, k=3):
+        super().__init__()
+        self.k=k
+        self.conv1 = nn.Conv1d(k,64,1)
+        self.conv2 = nn.Conv1d(64,128,1)
+        self.conv3 = nn.Conv1d(128,1024,1)
+        self.fc1 = nn.Linear(1024,512)
+        self.fc2 = nn.Linear(512,256)
+        self.fc3 = nn.Linear(256,k*k)
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(256)
+
+
+    def forward(self, input):
+        # input.shape == (bs,n,3)
+        bs = input.size(0)
+        xb = F.relu(self.bn1(self.conv1(input)))
+        xb = F.relu(self.bn2(self.conv2(xb)))
+        xb = F.relu(self.bn3(self.conv3(xb)))
+        pool = nn.MaxPool1d(xb.size(-1))(xb)
+        flat = nn.Flatten(1)(pool)
+        xb = F.relu(self.bn4(self.fc1(flat)))
+        xb = F.relu(self.bn5(self.fc2(xb)))
+
+        #initialize as identity
+        init = torch.eye(self.k, requires_grad=True).repeat(bs,1,1)
+        if xb.is_cuda:
+            init=init.cuda()
+        matrix = self.fc3(xb).view(-1,self.k,self.k) + init
+        return matrix
+
+
+class Transform(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.input_transform = Tnet(k=3)
+        self.feature_transform = Tnet(k=64)
+        self.conv1 = nn.Conv1d(3,64,1)
+
+        self.conv2 = nn.Conv1d(64,128,1)
+        self.conv3 = nn.Conv1d(128,1024,1)
+
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+
+    def forward(self, input):
+        matrix3x3 = self.input_transform(input)
+        # batch matrix multiplication
+        xb = torch.bmm(torch.transpose(input,1,2), matrix3x3).transpose(1,2)
+
+        xb = F.relu(self.bn1(self.conv1(xb)))
+
+        matrix64x64 = self.feature_transform(xb)
+        xb = torch.bmm(torch.transpose(xb,1,2), matrix64x64).transpose(1,2)
+
+        xb = F.relu(self.bn2(self.conv2(xb)))
+        xb = self.bn3(self.conv3(xb))
+        xb = nn.MaxPool1d(xb.size(-1))(xb)
+        output = nn.Flatten(1)(xb)
+        return output, matrix3x3, matrix64x64
+
+class PointNet(nn.Module):
+    def __init__(self, classes = 10):
+        super().__init__()
+        self.transform = Transform()
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, classes)
+        
+
+        self.bn1 = nn.BatchNorm1d(512)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.dropout = nn.Dropout(p=0.3)
+        self.logsoftmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input):
+        xb, matrix3x3, matrix64x64 = self.transform(input)
+        xb = F.relu(self.bn1(self.fc1(xb)))
+        xb = F.relu(self.bn2(self.dropout(self.fc2(xb))))
+        output = self.fc3(xb)
+        return self.logsoftmax(output), matrix3x3, matrix64x64
+    
+
+def pointnetloss(outputs, labels, m3x3, m64x64, alpha = 0.0001):
+    criterion = torch.nn.NLLLoss()
+    bs=outputs.size(0)
+    id3x3 = torch.eye(3, requires_grad=True).repeat(bs,1,1)
+    id64x64 = torch.eye(64, requires_grad=True).repeat(bs,1,1)
+    if outputs.is_cuda:
+        id3x3=id3x3.cuda()
+        id64x64=id64x64.cuda()
+    diff3x3 = id3x3-torch.bmm(m3x3,m3x3.transpose(1,2))
+    diff64x64 = id64x64-torch.bmm(m64x64,m64x64.transpose(1,2))
+    return criterion(outputs, labels) + alpha * (torch.norm(diff3x3)+torch.norm(diff64x64)) / float(bs)
+
+
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
